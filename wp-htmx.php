68d95ca95c8362cdcec540150524a379ff62eba8
stefano <sgaruti@gmail.com>
2026-05-25 10:52:21 +0200

    scaffolding

---
+++ wp-htmx.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Htmx
+ * Plugin URI:      https://github.com/garubi/wp-htmx
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Stefano Garuti
+ * Author URI:      https://github.com/garubi
+ * Text Domain:     wp-htmx
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Htmx
+ */
+
+// Your code starts here.
